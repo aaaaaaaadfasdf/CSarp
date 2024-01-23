dc8de867cce086c004e55ec96da02b21e4abcce7
Nathan <jonathan.rigo01@gmail.com>
2024-01-23 06:51:25 +0100

    this is a commit

---
+++ Project 1/ConsoleApp1/Imput.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.Data;
+
+
+
+partial  class    MainProgram: Form
+{
+
+public class Imput{
+
+    
+void DirectionV( int x, int y){
+    int c=0;
+
+       while(true){
+        c+=1;
+        if(!(Map[x+c,y]==0)){
+            break;
+        }
+       }
+}
+   
+
+}
+
+}
