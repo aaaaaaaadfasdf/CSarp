c64312ebac013717584f68553a6dbce1ba2d5c2c
Nathan <jonathan.rigo01@gmail.com>
2024-01-25 08:39:13 +0100

    commit 25.01.2024

--- Project 1/ConsoleApp1/obj/Debug/net8.0-windows/ConsoleApp1.AssemblyInfo.cs
+++ Project 1/ConsoleApp1/obj/Debug/net8.0-windows/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+911c95e5323fd64b244561fea5c3e977a02cc186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f271a35a061dc3e2b08234d030704854d852a552")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
