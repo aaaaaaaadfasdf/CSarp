0cfb7642297327c24d706327d59f3eb0fa70007f
Nathan <jonathan.rigo01@gmail.com>
2024-03-21 20:46:38 +0100

    commitThis

--- Project 1/ConsoleApp1/Animate.cs
+++ Project 1/ConsoleApp1/Animate.cs
@@ -1,105 +1,13 @@
 using System.Drawing.Drawing2D;
 
 
-static class Grid
-{
-
-    public static int x = Control.gridx;
-    public static int y = Control.gridy;
-
-    public static int block = 1;
-
-    public static int air = 0;
-    public static float space;
-
-    public static int[,] Map;
-
-    public static void MakeGrid()
-    {
-        Map = new int[x, y];
-        for (int i = 0; i < x; i++)
-        {
-            for (int j = 0; j < y; j++)
-            {
-
-                Map[i, j] = 0;
-
-
-
-
-            }
-        }
-
-        // Set up where the lines are
-
-        for (int i = 10; i < x - 10; i++)
-        {
-            for (int j = 10; j < y - 10; j++)
-            {
-
-
-
-
-
-            }
-        }
-
-
-
-        for (int i = 0; i < x - 0; i += 20)
-        {
-            for (int j = 0; j < y - 0; j++)
-            {
-
-                
-                            if(j%30<27){
-                            Map[i,j] = block;
-                             }
-
-                            if((x-j)%30<27){
-                            Map[i+10,j] = block;
-                             }
-
-                
-            }
-
-
-        }
-
-
-        for (int i = 0; i < x - 0; i++)
-        {
-            for (int j = 0; j < y - 0; j += 10)
-            {
-
-                
-                            if(i%10<6){
-                            Map[i,j] = block;
-                             }
-
-                            if((x-i)%10<2){
-                            Map[i,j+2] = block;
-                             }
-                
-
-            }
-
-
-        }
-
-    }
-
-
-
-}
-
 
 
 struct Window
 {
 
-    public static int x = Control.size;
-    public static int y = Control.size;
+    public static int x = Control.sizex;
+    public static int y = Control.sizey;
 }
 class MainProgram : Form
 {
